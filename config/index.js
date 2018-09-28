79707f617cef92916b55ccd2bfb1a8929b51f86e
WANIN28\weichech.huang <king17610@gmail.com>
2018-09-28 10:15:54 +0800

    gitignore remove /dist

--- config/index.js
+++ config/index.js
@@ -42,8 +42,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'PokemonKing/dist/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/PokemonKing/dist/',
 
     /**
      * Source Maps
